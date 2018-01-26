b75a296de7f6b2d470b47adfc833cd030fabc2f4
taitoan3027 <taitoan3027@gmail.com>
2018-01-26 23:14:57 +0700

    Add files via upload

--- DCTag/examples/manage_folder.c
+++ DCTag/examples/manage_folder.c
@@ -1,33 +1,44 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<stdint.h>
 #include<string.h>
 #include<dirent.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <time.h>
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <limits.h>
 
 int remove_directory(const char *path);
-
+char** str_split(char* a_str, const char a_delim);
+int GetMaxDay();
+uint32_t max_day = 0;
 int main()
-{	time_t rawtime;
-	time ( &rawtime );
-    struct tm *info;
-	info = localtime( &rawtime );
-	
-	
-	
-	char newfilename[100];
-	char oldfilename[100];
-	sprintf(newfilename, "%04d_%02d_%02d", (info->tm_year) + 1900, (info->tm_mon) + 1, info->tm_mday);
-	
-	struct tm *old_info;
-	time_t old_time = rawtime - 2592000;
-	old_info = localtime( &old_time );
-	sprintf(oldfilename, "%04d_%02d_%02d", (old_info->tm_year) + 1900, (old_info->tm_mon) + 1, old_info->tm_mday);
-	
-	int result = mkdir(newfilename, 0777);
-	remove_directory(oldfilename);
+{	
+	if((max_day = GetMaxDay()) != 0)
+	{
+		time_t rawtime;
+		time ( &rawtime );
+		struct tm *info;
+		info = localtime( &rawtime );
+		
+		
+		
+		char newfilename[100];
+		char oldfilename[100];
+		sprintf(newfilename, "%04d_%02d_%02d", (info->tm_year) + 1900, (info->tm_mon) + 1, info->tm_mday);
+		
+		struct tm *old_info;
+		time_t old_time = rawtime - max_day * 86400;
+		old_info = localtime( &old_time );
+		sprintf(oldfilename, "%04d_%02d_%02d", (old_info->tm_year) + 1900, (old_info->tm_mon) + 1, old_info->tm_mday);
+		
+		int result = mkdir(newfilename, 0777);
+		remove_directory(oldfilename);
+	}
 	return 0;
  }
 
@@ -91,4 +102,82 @@ int main()
    }
 
    return r;
+}
+int GetMaxDay()
+{
+	int i_day;
+    FILE *r_fptr; // pointer to config file
+	char row[100];
+	if ((r_fptr = fopen("client_config.txt", "r")) == NULL)
+    {
+        printf("Error! can't found config file");  
+		return 0;
+    }
+	else
+	{
+		 // reads text until newline 
+		while (fgets( row, sizeof( row ), r_fptr ) != NULL ) 
+		{
+			char** frame_parts;
+			frame_parts = str_split(row, ',');
+			if(frame_parts)
+			{
+				if(strcmp(*(frame_parts), "day") == 0)
+				{
+					
+					i_day = atoi(*(frame_parts+1));
+					printf("Set Max Storage timestamp (day): %d", i_day);
+					//sprintf(server_username,"%s", *(frame_parts+1));
+				}
+			}
+		}
+		return i_day;
+	}
+}
+char** str_split(char* a_str, const char a_delim)
+{
+    char** result    = 0;
+    size_t count     = 0;
+    char* tmp        = a_str;
+    char* last_comma = 0;
+    char delim[2];
+    delim[0] = a_delim;
+    delim[1] = 0;
+
+    /* Count how many elements will be extracted. */
+    while (*tmp)
+    {
+        if (a_delim == *tmp)
+        {
+            count++;
+            last_comma = tmp;
+        }
+        tmp++;
+    }
+
+    /* Add space for trailing token. */
+    count += last_comma < (a_str + strlen(a_str) - 1);
+
+    /* Add space for terminating null string so caller
+       knows where the list of returned strings ends. */
+    count++;
+
+    result = malloc(sizeof(char*) * count);
+
+    if (result)
+    {
+        size_t idx  = 0;
+        char* token = strtok(a_str, delim);
+
+        while (token)
+        {
+            assert(idx < count);
+            *(result + idx++) = strdup(token);
+            token = strtok(0, delim);
+        }
+        assert(idx == count - 1);
+        *(result + idx) = 0;
+    }
+
+    return result;
 }
--- DCTag/examples/read_tag.c
+++ DCTag/examples/read_tag.c
@@ -7,223 +7,133 @@
 #include <assert.h>
 
 
-#define MAX_TAG_COUNT					100
+#define MAX_TAG_COUNT					20
 void LookupUnit(char* pchr_tag_name, char* pchr_result);
 char** str_split(char* a_str, const char a_delim);
+void ReadTag( FILE * fptr, char * tag_names, char* unit, char scale, char* min, char* max);
 
-int main ( int argc, char * argv[] )
+int main ()
 {
-	char ** tag_names;
+	char *tag_names[ MAX_TAG_COUNT ];
+	char *tag_units[ MAX_TAG_COUNT ];
 	DC_TAG_HANDLE tag_handles[ MAX_TAG_COUNT ];
 	DC_TAG tag_values[ MAX_TAG_COUNT ];
 	int rc;
 	int i;
 	int retry;
-	int tag_count = 0;
+	int tag_cnt = 0;
+	char ret_str[100];
 	
-	if ( argc < 2 )
+	FILE *ptr_trc_file;
+	//read tag_list
+	if ((ptr_trc_file = fopen("tag_read_config.txt", "r")) != NULL)
 	{
-		if ( !isatty( fileno( stdin ) ) )
-		{
-			// try to read the tag name from stdin
-			char * line;
-			size_t n;
-			ssize_t len;
-			
-			tag_names = calloc( MAX_TAG_COUNT + 1, sizeof( char* ) );
-			if ( NULL == tag_names )
-			{
-				printf( "alloc memory error!\n" );
-				return -1;
-			}
-			
-			do
-			{
-				line = NULL;
-				n = 0;
-				len = getline( &line, &n, stdin );
-				if ( len > 1 )
-				{
-					while ( len > 1
-						&& ( line[ len - 1 ] == '\x0d' || line[ len - 1 ] == '\x0a' ) )
-					{
-						line[ len - 1 ] = 0;
-						len--;
-					}
-				}
-
-				if ( len > 0 )
-				{
-					tag_names[ tag_count++ ] = line;
-				}
-			} while ( len != -1 && tag_count < MAX_TAG_COUNT );
-		}
-
-		if ( tag_count <= 0 )
-		{
-			printf( "usage: dc_tag_read <tag_name> ...\n" );
-			printf( "    or echo \"<tag_name>\" ... | dc_tag_read\n" );
-			printf( "    or cat tag_list.txt | dc_tag_read\n" );
-			printf( "    or dc_tag_read < tag_list.txt\n" );
-			return -2;
-		}
-	}
-	else
-	{
-		tag_names = &argv[ 1 ];
-		tag_count = argc - 1;
-	}
-
-	if ( argc > MAX_TAG_COUNT )
-	{
-		printf( "too many tag name specified, the max tag count is %d!\n", MAX_TAG_COUNT );
-		return -3;
-	}
-
-	memset( tag_handles, 0, sizeof( tag_handles ) );
-	memset( tag_values, 0, sizeof( tag_values ) );
-	
-	// initialize the DCTag SDK
-	rc = dc_tag_init( MAX_TAG_COUNT, NULL, NULL );
-	if ( rc != 0 )
-	{
-		printf( "dc_tag_init failed! error = %d\n", rc );
-		return -4;
-	}
-
-	// prepare the tag handles
-	for ( i = 0; i < tag_count; i++ )
-	{
-		tag_handles[ i ] = dc_tag_open( tag_names[ i ], NULL );
-		if ( NULL == tag_handles[ i ] )
-		{
-			printf( "fail to open tag: %s\n", tag_names[ i ] );
-			dc_tag_uninit();
-			return -5;
-		}
-	}
-
-	// read tags
-	retry = 50;
-	do
-	{
-		usleep( 100000 );
-		rc = dc_tag_read( tag_handles, tag_values, tag_count );
-	} while ( rc != 0 && retry-- );
-
-	if ( rc != 0 )
-	{
-		printf( "fail to read tag value! error = %d\n", rc );
-		for ( i = 0; i < tag_count; i++ )
-		{
-			dc_tag_close( tag_handles[ i ] );
-		}
-		dc_tag_uninit();
-		return -6;
-	}
-
-	FILE *ptr_file;
-	
-	time_t f_time ;
-	time(&f_time);
-	struct tm *f_tm ;
-	f_tm = localtime(&f_time);
-	char filename[50];
-	sprintf(filename,"%04d_%02d_%02d/temp.txt", (f_tm->tm_year)+1900, (f_tm->tm_mon)+1, f_tm->tm_mday);
-	ptr_file =fopen(filename, "w");
-	
-	
-	if (!ptr_file)
-		return 1;
-	
-	printf( "\n%-16s%-16s%-8s%s\n", "Tag Name", "Value", "Quality", "Time Stamp (UTC)" );
-	printf( "--------------- --------------- ------- ---------------------------------------\n" );
-	
-	for ( i = 0; i < tag_count; i++ )
-	{
-		time_t tt = tag_values[ i ].time;
-		struct tm * gmt = localtime( &tt );
-		printf(
-			"%-16s%-16lf%04xH\t%04d-%02d-%02d %02d:%02d:%02d (%llu.%06u)\n",
-			tag_names[ i ],
-			tag_values[ i ].value,
-			(uint16_t) tag_values[ i ].quality,
-			1900 + gmt->tm_year,
-			gmt->tm_mon + 1,
-			gmt->tm_mday,
-			gmt->tm_hour,
-			gmt->tm_min,
-			gmt->tm_sec,
-			tag_values[ i ].time,
-			tag_values[ i ].usec );
-			
-			//get unit from lookup file
-			//memset(p_unit,0,sizeof(p_unit));			
-			char p_unit[5];
-			LookupUnit(tag_names[ i ], p_unit);
-			
-		fprintf(ptr_file,
-			"%s\t%5.6lf\t%s\t%04d%02d%02d%02d%02d%02d\t%04xH\r\n",
-			tag_names[ i ],
-			tag_values[ i ].value,
-			p_unit,
-			1900 + gmt->tm_year,
-			gmt->tm_mon + 1,
-			gmt->tm_mday,
-			gmt->tm_hour,
-			gmt->tm_min,
-			gmt->tm_sec,
-			(uint16_t) tag_values[ i ].quality );
-	}
-	
-	printf( "\n" );
-
-	// close tag handles
-	for ( i = 0; i < tag_count; i++ )
-	{
-		dc_tag_close( tag_handles[ i ] );
-	}
-
-	// uninitialize the SDK
-	dc_tag_uninit();
-	fclose(ptr_file);
-	
-	return 0;
-}
-void LookupUnit(char* pchr_tag_name, char* pchr_result)
-{
-	// open unit config file
-	FILE *ptr_unitfile;
-	if ((ptr_unitfile = fopen("unit_config.txt", "r")) != NULL)
-	{
-		char row[50];
-		char c_ufound = 0;
-
-		while (fgets( row, sizeof( row ), ptr_unitfile ) != NULL ) 
+		char row[100];	
+		FILE *ptr_file;
+				
+		time_t f_time ;
+		time(&f_time);
+		struct tm *f_tm ;
+		f_tm = localtime(&f_time);
+		char filename[50];
+		sprintf(filename,"%04d_%02d_%02d/temp.txt", (f_tm->tm_year)+1900, (f_tm->tm_mon)+1, f_tm->tm_mday);
+		ptr_file =fopen(filename, "w");
+		
+		while (fgets( row, sizeof( row ), ptr_trc_file ) != NULL ) 
 		{
 			char** frame_parts;
 			frame_parts = str_split(row, ',');
 			if(frame_parts)
-			{
-				if(strcmp(pchr_tag_name, *(frame_parts))==0)
-				{
-					c_ufound = 1;
-					sprintf(pchr_result,"%s", *(frame_parts+1));
-				}
+			{			
+				tag_names[tag_cnt] = *(frame_parts);
+				tag_units[tag_cnt++] = *(frame_parts + 1);
 			}
 		}
-		if (c_ufound == 0)
+		fclose(ptr_trc_file);
+		// limit tag num
+		if ( tag_cnt > MAX_TAG_COUNT + 1 )
 		{
-			sprintf(pchr_result,"-");
+			printf( "too many tag name specified, the max tag count is %d!\n", MAX_TAG_COUNT );
+			return -3;
 		}
-		fclose(ptr_unitfile);
+		// init to read tag
+		memset( tag_handles, 0, sizeof( tag_handles ) );
+		memset( tag_values, 0, sizeof( tag_values ) );
+		
+		rc = dc_tag_init( MAX_TAG_COUNT, NULL, NULL );
+		if ( rc != 0 )
+		{
+			printf( "dc_tag_init failed! error = %d\n", rc );
+			return -4;
+		}
+
+		// prepare the tag handles
+		for ( i = 0; i < tag_cnt; i++ )
+		{
+			tag_handles[ i ] = dc_tag_open( tag_names[ i ], NULL );
+			if ( NULL == tag_handles[ i ] )
+			{
+				printf( "fail to open tag: %s\n", tag_names[ i ] );
+				dc_tag_uninit();
+				return -5;
+			}
+		}
+		//read tags
+		retry = 50;
+		do
+		{
+			usleep( 100000 );
+			rc = dc_tag_read( tag_handles, tag_values, tag_cnt );
+		} while ( rc != 0 && retry-- );
+
+		if ( rc != 0 )
+		{
+			printf( "fail to read tag value! error = %d\n", rc );
+			for ( i = 0; i < tag_cnt; i++ )
+			{
+				dc_tag_close( tag_handles[ i ] );
+			}
+			dc_tag_uninit();
+			return -6;
+		}
+		for ( i = 0; i < tag_cnt; i++ )
+		{
+			time_t tt = tag_values[i].time;
+			struct tm * gmt = gmtime( &tt );
+			sprintf(ret_str,
+				"%s\t%5.6lf\t%s\t%04d%02d%02d%02d%02d%02d\t%04xH\r\n",
+				tag_names[i],
+				tag_values[i].value,
+				tag_units[i],
+				1900 + gmt->tm_year,
+				gmt->tm_mon + 1,
+				gmt->tm_mday,
+				gmt->tm_hour,
+				gmt->tm_min,
+				gmt->tm_sec,
+				(uint16_t) tag_values[i].quality );
+
+			fprintf(ptr_file, ret_str);
+			printf( ret_str);
+		}
+		// close tag handles
+		for ( i = 0; i < tag_cnt; i++ )
+		{
+			dc_tag_close( tag_handles[ i ] );
+		}
+
+		// uninitialize the SDK
+		dc_tag_uninit();
+		fclose(ptr_file);
 	}
 	else
 	{
-		sprintf(pchr_result,"!!!");
+		printf("file \"tag_read_config.txt\" not found!!!");
+		return 0;
 	}
-		
+	return 0;
 }
+
 char** str_split(char* a_str, const char a_delim)
 {
     char** result    = 0;
@@ -270,4 +180,84 @@ char** str_split(char* a_str, const char a_delim)
     }
 
     return result;
+}
+void ReadTag( FILE * fptr, char * tag_names, char* unit, char scale, char* min, char* max)
+{
+	DC_TAG_HANDLE tag_handles;
+	DC_TAG tag_values;
+	int rc;
+	
+	int retry;
+	int tag_count = 0;
+	char ret_str[100];	
+	tag_count = 1;
+	
+	
+	// initialize the DCTag SDK
+	rc = dc_tag_init( MAX_TAG_COUNT, NULL, NULL );
+	if ( rc != 0 )
+	{
+		sprintf( ret_str, "dc_tag_init failed! error = %d\r\n", rc );
+		fprintf(fptr, ret_str);
+		printf( ret_str);
+		return 0;
+	}
+
+	// prepare the tag handles
+	
+		tag_handles = dc_tag_open( tag_names, NULL );
+		if ( NULL == &tag_handles )
+		{
+			sprintf( ret_str,"fail to open tag: %s\r\n", tag_names );
+			dc_tag_uninit();
+			fprintf(fptr, ret_str);
+			printf( ret_str);
+			return 0;
+		}
+	
+
+	// read tags
+	retry = 50;
+	do
+	{
+		usleep( 100000 );
+		rc = dc_tag_read( &tag_handles, &tag_values, tag_count );
+	} while ( rc != 0 && retry-- );
+
+	if ( rc != 0 )
+	{
+		sprintf(ret_str, "fail to read tag value! error = %d\r\n", rc );
+		
+		dc_tag_close( tag_handles);
+		
+		dc_tag_uninit();
+		fprintf(fptr, ret_str);
+		printf( ret_str);
+		return 0;
+	}	
+	time_t tt = tag_values.time;
+	struct tm * gmt = gmtime( &tt );
+	sprintf(ret_str,
+		"%s\t%5.6lf\t%s\t%04d%02d%02d%02d%02d%02d\t%04xH\r\n",
+		tag_names,
+		tag_values.value,
+		unit,
+		1900 + gmt->tm_year,
+		gmt->tm_mon + 1,
+		gmt->tm_mday,
+		gmt->tm_hour,
+		gmt->tm_min,
+		gmt->tm_sec,
+		(uint16_t) tag_values.quality );
+
+	// close tag handles
+
+	dc_tag_close( tag_handles );
+
+
+	// uninitialize the SDK
+	dc_tag_uninit();
+	fprintf(fptr, ret_str);
+	printf( ret_str);
+	return ret_str;
 }
